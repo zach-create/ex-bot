4c17eb79e6aefa1a8d09709fc758b22a6b4238ca
zach-create <68110635+zach-create@users.noreply.github.com>
2020-07-10 22:34:37 +0530

    removed embebed

--- index.js
+++ index.js
@@ -37,37 +37,6 @@ bot.on('message', msg=>{
 
 
 
-const PREFIX ='!';
-
-
-bot.on('ready', () =>{
-let args = message.content.substring(PREFIX.length).split("");
- switch(args[0]){
-
-    case 'servers':
-        const embed = new Discord.MessageEmbed()
-        .setTitle('INDINET CSGO SERVER IP')
-        .addField('DEATHMATCH','steam://connect/45.117.204.250:27015').addField('PUBLIC CASUAL','steam://connect/45.117.204.250:27016')
-        .addField('AWP SERVER','steam://connect/45.117.204.250:27017')
-        .addField('RETAKE 1','steam://connect/103.57.241.226:28015')
-        .addField('RETAKE 3','steam://connect/45.117.203.250:28015')
-        .addField('MULTI 1 V 1 ','steam://connect/45.117.203.250:27017')
-        .addField('KZ-CLIMB','steam://connect/45.117.203.250:27016')
-        .addField('SCRIM 2','steam://connect/103.57.241.226:28016')
-        .setColor(0xFEFA07)
-        message.channel.send(embed);
-     break;
-
-
-
-
-
- }
-
-
-})
-
-
 
 
 
