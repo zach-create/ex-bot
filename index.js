4f972f8f444fe6cf8edf6f092c9823878c3a3d6a
zach-create <68110635+zach-create@users.noreply.github.com>
2020-07-10 21:58:15 +0530

    fully updated

--- index.js
+++ index.js
@@ -11,59 +11,32 @@ console.log('This Bot is Online');
 
 
 
-const PREFIX = "!";
-
-bot.on('message', message => {
-    let args = message.content.slice(PREFIX.length).split(" ");
-    switch(args[0]) {
-      case 'servers':
-          const embed = new Discord.MessageEmbed()
-          .setTitle('INDINET CSGO SERVER IP')
-          .addField('DEATHMATCH','steam://connect/45.117.204.250:27015').addField('PUBLIC CASUAL','steam://connect/45.117.204.250:27016')
-          .addField('AWP SERVER','steam://connect/45.117.204.250:27017')
-          .addField('RETAKE 1','steam://connect/103.57.241.226:28015')
-          .addField('RETAKE 3','steam://connect/45.117.203.250:28015')
-          .addField('MULTI 1 V 1 ','steam://connect/45.117.203.250:27017')
-          .addField('KZ-CLIMB','steam://connect/45.117.203.250:27016')
-          .addField('SCRIM 2','steam://connect/103.57.241.226:28016')
-          .setColor(0xFEFA07);
-          ;
-          message.channel.send(embed);
-          break;
 
 
 
+
+bot.on('message', msg=>{
+    if(msg.content === "!servers"){
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle('INDINET CSGO SERVER IP')
+        .addField('DEATHMATCH','steam://connect/45.117.204.250:27015').addField('PUBLIC CASUAL','steam://connect/45.117.204.250:27016')
+        .addField('AWP SERVER','steam://connect/45.117.204.250:27017')
+        .addField('RETAKE 1','steam://connect/103.57.241.226:28015')
+        .addField('RETAKE 3','steam://connect/45.117.203.250:28015')
+        .addField('MULTI 1 V 1 ','steam://connect/45.117.203.250:27017')
+        .addField('KZ-CLIMB','steam://connect/45.117.203.250:27016')
+        .addField('SCRIM 2','steam://connect/103.57.241.226:28016')
+        .setColor(0xFEFA07);
+        ;
+        message.channel.send(embed);
+
+
+     msg.reply('HELLO FRIEND!');
+    
     }
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
+    
 
 bot.on('message', msg=>{
 if(msg.content === "HELLO"){
