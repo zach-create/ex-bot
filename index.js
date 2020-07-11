bd9c32944af0d370b33bb2fb18aebe0791dc7cfc
zach-create <68110635+zach-create@users.noreply.github.com>
2020-07-11 22:41:25 +0530

    updatded

--- index.js
+++ index.js
@@ -66,73 +66,87 @@ const PREFIX = '+';
 
 bot.on('message', message => {
 
-    let args = message.content.substring(PREFIX.length).split(" ");
+    let args = message.content.substring(PREFIX.length).split("");
     switch (args[0]) {
         case 'kick':
 
-                    if(message.member.roles.find(r => r.name ==="HEAD ADMIN"))  
-                    {
-
-                        message.reply('!pong')
-                    }else if(message.member.roles.find(r => r.name ==="ADMIN"))
-                    {
-
-                        message.reply('!pong')
+            if (message.member.roles.find(r => r.name === "HEAD ADMIN")) {
 
 
-                    }else{
-                    
-                    return message.channel.send('YOU DO NOT HAVE THE PERMISSION')
-                    
+                const user = message.mentions.users.first();
+                if (user) {
+                    const member = message.guild.member(user);
+                    if (member) {
+
+                        member.kick('You Were Been Kicked from Indinet Official Discord Server').then(() => {
+                            message.reply(`Successfully Kicked  ${user.tag}`);
+                        }).catch(err => {
+
+                            message.reply('i was Uynable to kick the user');
+                            console.log(err);
+
+
+                        });
+
+                    } else {
+
+                        message.reply("That Usser isint in the Server Right Now")
+
+
                     }
-                    
-                    
-                    
-
-
-/*
-
-            const user = message.mentions.users.first();
-            if (user) {
-                const member = message.guild.member(user);
-                if (member) {
-
-                    member.kick('You Were Been Kicked from Indinet Official Discord Server').then(() => {
-                        message.reply(`Successfully Kicked  ${user.tag}`);
-                    }).catch(err => {
-
-                        message.reply('i was Uynable to kick the user');
-                        console.log(err);
-
-
-                    });
-
                 } else {
-
-                    message.reply("That Usser isint in the Server Right Now")
-
+                    message.reply('You Need to Specify A Person')
 
                 }
+
+            } else if (message.member.roles.find(r => r.name === "ADMIN")) {
+
+
+                const user = message.mentions.users.first();
+                if (user) {
+                    const member = message.guild.member(user);
+                    if (member) {
+
+                        member.kick('You Were Been Kicked from Indinet Official Discord Server').then(() => {
+                            message.reply(`Successfully Kicked  ${user.tag}`);
+                        }).catch(err => {
+
+                            message.reply('I was Unable to kick the user');
+                            console.log(err);
+
+
+                        });
+
+                    } else {
+
+                        message.reply("That User isint in the Server Right Now")
+
+
+                    }
+                } else {
+                    message.reply('You Need to Specify A Person')
+
+                }
+
             } else {
-                message.reply('You Need to Specify A Person')
+
+                return message.channel.send('YOU DO NOT HAVE THE PERMISSION')
 
             }
-
-*/
-            break;
-            case 'ban':
+        break;
+        case 'ban':
             const userr = message.mentions.users.first();
             if (userr) {
                 const member = message.guild.member(userr);
                 if (member) {
 
-                      member.ban({ression:'You Were Banned From The Server' }).then(() =>{
-                          message.reply(`We Banned The Player ! ${userr.tag}`)
+                    member.ban({ ression: 'You Were Banned From The Server' }).then(() => {
+                        message.reply(`We Banned The Player ! ${userr.tag}`)
 
 
 
 
-                      })
+                    })
                 } else {
 
                     message.reply("That Usser isint in the Server Right Now")
@@ -144,7 +158,7 @@ bot.on('message', message => {
 
             }
 
-break;
+            break;
 
 
 
