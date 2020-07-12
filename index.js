c537a1141a59d800e30ffceb66c506e80f727952
zach-create <68110635+zach-create@users.noreply.github.com>
2020-07-12 11:30:32 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const token = 'NzMxMDMwODY2MTg4MzA0Mzk1.XwiPHQ.rzdDoCcgdxL21Li5Rsk8-rcHy4A';
 bot.on('ready', () => {
 
     console.log('This Bot is Online');
-    bot.user.setActivity('68,396 Players',{ type: 'WATCHING'}).catch(console.error);
+    bot.user.setActivity('68,396 Players', { type: 'WATCHING' }).catch(console.error);
 
 })
 
@@ -73,7 +73,7 @@ bot.on('message', message => {
     switch (args[0]) {
         case 'kick':
 
-            if (message.member.roles.find(r => r.name === "HEAD ADMIN")) {
+            if (message.member.roles.find(r => r.name === "GOD")) {
 
 
                 const user = message.mentions.users.first();
@@ -102,7 +102,36 @@ bot.on('message', message => {
 
                 }
 
-            } else if (message.member.roles.find(r => r.name === "ADMIN")) {
+            } else if (message.member.roles.find(r => r.name === "Head Admin")) {
+
+
+                const user = message.mentions.users.first();
+                if (user) {
+                    const member = message.guild.member(user);
+                    if (member) {
+
+                        member.kick('You Were Been Kicked from Indinet Official Discord Server').then(() => {
+                            message.reply(`Successfully Kicked  ${user.tag}`);
+                        }).catch(err => {
+
+                            message.reply('I was Unable to kick the user');
+                            console.log(err);
+
+
+                        });
+
+                    } else {
+
+                        message.reply("That User isint in the Server Right Now")
+
+
+                    }
+                } else {
+                    message.reply('You Need to Specify A Person')
+
+                }
+
+            } else if (message.member.roles.find(r => r.name === "BOTS")) {
 
 
                 const user = message.mentions.users.first();
@@ -136,74 +165,100 @@ bot.on('message', message => {
                 return message.channel.send('```YOU DO NOT HAVE THE PERMISSION```')
 
             }
-        break;
+            break;
         case 'ban':
-            if (message.member.roles.find(r => r.name === "HEAD ADMIN")) {
+            if (message.member.roles.find(r => r.name === "GOD")) {
 
 
 
-            const userr = message.mentions.users.first();
-            if (userr) {
-                const member = message.guild.member(userr);
-                if (member) {
+                const userr = message.mentions.users.first();
+                if (userr) {
+                    const member = message.guild.member(userr);
+                    if (member) {
 
-                    member.ban({ ression: 'You Were Banned From The Server' }).then(() => {
-                        message.reply(`We Banned The Player ! ${userr.tag}`)
-
-
+                        member.ban({ ression: 'You Were Banned From The Server' }).then(() => {
+                            message.reply(`We Banned The Player ! ${userr.tag}`)
 
 
-                    })
+
+
+                        })
+                    } else {
+
+                        message.reply("That Usser isint in the Server Right Now")
+
+
+                    }
                 } else {
-
-                    message.reply("That Usser isint in the Server Right Now")
-
+                    message.reply('You Need to Specify A Person')
 
                 }
-            } else {
-                message.reply('You Need to Specify A Person')
 
-            }
+            } else if (message.member.roles.find(r => r.name === "Head Admin")) {
 
-        }else if(message.member.roles.find(r => r.name === "HEAD ADMIN")) {
+                const userr = message.mentions.users.first();
+                if (userr) {
+                    const member = message.guild.member(userr);
+                    if (member) {
 
-            const userr = message.mentions.users.first();
-            if (userr) {
-                const member = message.guild.member(userr);
-                if (member) {
-
-                    member.ban({ ression: 'You Were Banned From The Server' }).then(() => {
-                        message.reply(`We Banned The Player ! ${userr.tag}`)
+                        member.ban({ ression: 'You Were Banned From The Server' }).then(() => {
+                            message.reply(`We Banned The Player ! ${userr.tag}`)
 
 
 
 
-                    })
+                        })
+                    } else {
+
+                        message.reply("That Usser isint in the Server Right Now")
+
+
+                    }
                 } else {
-
-                    message.reply("That Usser isint in the Server Right Now")
-
+                    message.reply('You Need to Specify A Person')
 
                 }
+
+
+
+
+
+
+
+            } else if (message.member.roles.find(r => r.name === "BOTS")) {
+
+
+                const user = message.mentions.users.first();
+                if (user) {
+                    const member = message.guild.member(user);
+                    if (member) {
+
+                        member.kick('You Were Been Kicked from Indinet Official Discord Server').then(() => {
+                            message.reply(`Successfully Kicked  ${user.tag}`);
+                        }).catch(err => {
+
+                            message.reply('I was Unable to kick the user');
+                            console.log(err);
+
+
+                        });
+
+                    } else {
+
+                        message.reply("That User isint in the Server Right Now")
+
+
+                    }
+                } else {
+                    message.reply('You Need to Specify A Person')
+
+                }
+
             } else {
-                message.reply('You Need to Specify A Person')
+
+                return message.channel.send('```YOU DO NOT HAVE THE PERMISSION```')
 
             }
-
-
-
-
-
-
-
-        }else{
-
-
-
-            return message.channel.send('```YOU DO NOT HAVE THE PERMISSION```')
-
-
-        }
             break;
 
     }
