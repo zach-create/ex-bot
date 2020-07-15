3af4df87007237307bfc002609723e04e880bec4
zach-create <68110635+zach-create@users.noreply.github.com>
2020-07-15 10:12:37 +0530

    players

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const token = 'NzMxMDMwODY2MTg4MzA0Mzk1.XwiPHQ.rzdDoCcgdxL21Li5Rsk8-rcHy4A';
 bot.on('ready', () => {
 
     console.log('This Bot is Online');
-    bot.user.setActivity('68,396 Players', { type: 'WATCHING' }).catch(console.error);
+    bot.user.setActivity('69,470 Players', { type: 'WATCHING' }).catch(console.error);
 
 })
 
